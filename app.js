36874e4808c2139798dae51163e14824e5e9e606
laihuamin <1445509994@qq.com>
2017-12-01 09:28:20 +0800

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const app = new express()
+const indexRouter = require('./routes/index.js')
+const usersRouter = require('./routes/users.js')
 
-app.get('/', (req, res) => {
-    res.send("Hello Node");
-})
-
-app.get('/users/:name', (req, res) => {
-    res.send(`Hello ${req.params.name}`)
-})
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 app.listen(3000)
