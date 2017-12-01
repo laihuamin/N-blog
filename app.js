e20efbe7ff0ca7bbac5da0f4316ce597214ebf07
laihuamin <1445509994@qq.com>
2017-12-01 22:46:26 +0800

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+const path = require('path')
 const express = require('express')
 const app = new express()
 const indexRouter = require('./routes/index.js')
 const usersRouter = require('./routes/users.js')
+
+// 模版引擎存放目录
+app.set('views', path.join(__dirname, 'views'))
+// 设置模版引擎为ejs
+app.set('view engine', 'ejs')
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
