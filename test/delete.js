6d041fd3a47d3fba139955eb380027960ea2fe63
laihuamin <1445509994@qq.com>
2017-12-27 15:42:44 +0800

    delete

--- test/delete.js
+++ test/delete.js
@@ -1,5 +1,41 @@
-User.remove({username: 'i5ting for delete'}, (err, doc) => {
-    t.false(err);
-    t.is(doc.result.ok, 1);
-    t.is(doc.result.n, 1);
-});
+const mongoose = require('mongoose');
+
+const db = mongoose.connect('mongodb://127.0.0.1:27017/myblog');
+
+db.connection.on('errpr', () => {
+    console.log('数据库链接失败')
+})
+
+db.connection.on('open', () => {
+    console.log('数据库链接成功')
+})
+// 定义shcema骨架
+DescSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    }
+})
+// 根据骨架定义模型
+const Desc = mongoose.model('Desc', DescSchema);
+
+// 先删除
+Desc.remove({name: 'laihuamin'}, function(err) {
+    if(err) {
+        throw err;
+    } else {
+        console.log('删除成功')
+    }
+})
+// 如果删除成功之后，验证是否成功
+Desc.find({}, function(err, doc) {
+    if(err) {
+        throw err;
+    } else {
+        console.log(doc)
+    }
+})
